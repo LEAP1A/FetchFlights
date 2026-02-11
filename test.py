d44cae2dff588eab62a770263ddf97a68565612b
TUTU's DESKTOP <hz2852@nyu.edu>
2026-02-12 00:23:54 +0800

    fix: add gitignore

--- test.py
+++
@@ -1,8 +0,0 @@
-from FlightRadar24 import FlightRadar24API
-
-fr_api = FlightRadar24API()
-
-full_data = fr_api.get_airport_details('ZLXY', page=2)['airport']['pluginData']['schedule']
-arrivalsList = full_data['arrivals']['data']
-
-print(full_data)
